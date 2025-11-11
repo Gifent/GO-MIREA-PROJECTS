84184db86c57515a0573c46f74734fcc1c5ebb94
Andrey <85444692+Gifent@users.noreply.github.com>
2025-11-11 16:45:14 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	name := "Andrey"
-	fmt.Printf("Hello, " + name + "!")
+	var a int = 10
+	var b float64 = 3.14
+	var s string = "golang"
+	var ok bool = true
+
+	fmt.Println(a, b, s, ok)
+
 }
